fd69723a68e5ddd277f1fa168cb9b043ba1a4a7c
mrkos <mrkos@spsejecna.cz>
2025-01-30 10:23:56 +0100

    vytvoreni Arraylistu kytky a metody Zasazeni ktera pridava kytku do kytek

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class Farm {
+    ArrayList<Flower> kytky= new ArrayList<>();
 
+    public String Zasazeni(Flower r){
+        kytky.add(r);
+        return "kytka byla zasazena";
 
+    }
 
 }
