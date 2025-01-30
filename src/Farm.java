d00c01e47784768bfa8dc8128c03cf1c59adaa20
mrkos <mrkos@spsejecna.cz>
2025-01-30 10:35:24 +0100

    vytvoreni metoda Sklizeni ktera kdyz voroste kztka sklidi

--- src/Farm.java
+++ src/Farm.java
@@ -3,10 +3,18 @@ import java.util.ArrayList;
 public class Farm {
     ArrayList<Flower> kytky= new ArrayList<>();
 
-    public String Zasazeni(Flower r){
+    public String Zasazeni(Flower r) {
         kytky.add(r);
         return "kytka byla zasazena";
-
     }
+
+        public String Sklizeni(Flower r){
+        if(r.vyrostla=true){
+            kytky.remove(r);
+        }
+           return "sklizeno!!";
+        }
+
+
 
 }
