cb49b443868bc2570d4bea0c2484c697a13f1b37
mrkos <mrkos@spsejecna.cz>
2025-01-30 10:39:02 +0100

    Upraveni metody Zasazeni tak aby byla zavisla na plose kterou potrebuje k rustu

--- src/Farm.java
+++ src/Farm.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 
 public class Farm {
+    private double volnaplocha=100;
     ArrayList<Flower> kytky= new ArrayList<>();
 
     public String Zasazeni(Flower r) {
-        kytky.add(r);
+        if(volnaplocha>r.chanceOfGrowth) {
+            kytky.add(r);
+            volnaplocha-=r.chanceOfGrowth;
+        }
         return "kytka byla zasazena";
     }
 
